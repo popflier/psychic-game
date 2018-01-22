d1f260674dfb51d4053610eafec5150f368a230a
popflier <nico@popflier.com>
2018-01-21 23:41:23 -0700

    transfer from desktop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,67 @@
+//variable to hold the win count
 var win = 0;
-var losses = 0;
+//variable to hold the loss count
+var loss = 0;
+//variable to hold the guess count
+var guessCount = 10;
+//variable to hold the users guess
 var userGuess = ;
-var guessesNum = 10;
+//empty variable to hold the guesses the user has made
 var guesses = [];
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];  
-var storedLetter = "";    
+//variable to hold the possible letters the computer can pick from
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+//random letter that computer has chosen
+var storedLetter = "";  
 
+
+//this will run the function to have the computer generate a random letter
 getRandomLetter();
 
+//this will add the keys the user presses to the end of the guesses array
 guesses.push();
 
 function getRandomLetter() {
    storedLetter = letters[Math.floor(Math.random()*letters.length)]
 };
 
-document.onkeyup(){
-    var keyPressed = String.fromCharCode(event.keyCode).toLowerCase();
+//function that updates the wins on the page
+function updateWins() {
+    document.querySelector("#wins").innerHTML = win++;
+};
 
+//function that updates the losses on the page
+function updateLosses() {
+    document.querySelector("#losses").innerHTML = loss++;
+};
+
+function gameStatus() {
+    document.querySelector("#gameOver").innerHTML = "Game Over!";
+};
+
+
+document.onkeypress(){
+    var keyPressed = String.fromCharCode(event.keyCode).toLowerCase();
+    
+    //if guessCount is equal to or greater than 1 or less than or equal to 10 then get random letter (to start game). 
+    if (guessCount => 1 || guessCount <= 10) {
+        getRandomLetter ();    
+    }
+    //anything else then the guessCount would be 0 since it's less than 10 but more than 1. If it is 0 then the game is over.
+    else {
+        gameStatus();
+        //alert("Do you want to play again?");
+    };
+    
     if (userGuess === storedLetter){
-        document.getElementById("#wins") wins++
-        };
+        updateWins();
+        getRandomLetter();
+    };
+    
+    if (userGuess != storedLetter && guessCount === 0){
+        updateLosses();
+        getRandomLetter();
+    };
+    
     
         
         
