4fd8d0611257700f119f78c4f7b86104b15db548
popflier <nico@popflier.com>
2018-01-22 23:11:08 -0700

    transfer from Desktop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,48 @@
+//GLOBAL SPACE
+
 //variable to hold the win count
 var win = 0;
 //variable to hold the loss count
 var loss = 0;
 //variable to hold the guess count
 var guessCount = 10;
-//variable to hold the users guess
-var userGuess = ;
 //empty variable to hold the guesses the user has made
 var guesses = [];
 //variable to hold the possible letters the computer can pick from
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
-//random letter that computer has chosen
-var storedLetter = "";  
+//variable to hold the random letter the computer has chosen
+var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+
+//NON-GLOBAL SPACE
+document.onkeyup = function (event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    
+    if (guesses.indexOf(userGuess) < 0 && letters.indexOf(userGuess) >= 0) {
+        guesses[guesses.length] = userGuess;
+        guessCount --;
+    }
+    
+    if (guessCount >= 1 || guessCount <= 10) {
+        getRandomLetter ();    
+    }
+
+    else {
+        gameStatus();
+        //alert("Do you want to play again?");
+    };
+    
+    if (userGuess === computerGuess){
+        updateWins();
+    };
+    
+    if (userGuess != computerGuess && guessCount === 0){
+        updateLosses();
+    };
+       
+        
+  };
 
 
-//this will run the function to have the computer generate a random letter
-getRandomLetter();
-
-//this will add the keys the user presses to the end of the guesses array
-guesses.push();
-
-function getRandomLetter() {
-   storedLetter = letters[Math.floor(Math.random()*letters.length)]
-};
-
-//function that updates the wins on the page
 function updateWins() {
     document.querySelector("#wins").innerHTML = win++;
 };
@@ -39,34 +57,61 @@ function gameStatus() {
 };
 
 
-document.onkeypress(){
-    var keyPressed = String.fromCharCode(event.keyCode).toLowerCase();
-    
-    //if guessCount is equal to or greater than 1 or less than or equal to 10 then get random letter (to start game). 
-    if (guessCount => 1 || guessCount <= 10) {
-        getRandomLetter ();    
-    }
-    //anything else then the guessCount would be 0 since it's less than 10 but more than 1. If it is 0 then the game is over.
-    else {
-        gameStatus();
-        //alert("Do you want to play again?");
-    };
-    
-    if (userGuess === storedLetter){
-        updateWins();
-        getRandomLetter();
-    };
-    
-    if (userGuess != storedLetter && guessCount === 0){
-        updateLosses();
-        getRandomLetter();
-    };
-    
-    
-        
-        
-        
-        
-        
-  };
+
 //anytime need to store information to use create a variable for that
+
+//var win = 0;
+//var lose = 0;
+//var guessCount = 10;
+//var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+//var guessedLetters = [];
+//document.onkeypress = function(event) {
+//    var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+//    
+//    var userGuess = event.key;
+//    
+//    if (userGuess === computerGuess) {
+//        document.querySelector("#wins").innerHTML = wins++;
+//    }
+//    else {
+//        guessCount --;
+//    }
+//    if (userGuess !== computerGuess && guessCount === 0) {
+//        doument.querySelector("#losses").innerHTML = lose++;
+//    }
+//    
+//    console.log(userGuess);
+//};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
